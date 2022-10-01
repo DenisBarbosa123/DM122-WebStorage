b38c1217cef3e6f31e1829778c69ff0208f3f752
Denis Sebastião Costa Barbosa <denis.barbosa@inatel.br>
2022-10-01 16:07:25 -0300

    add dexie to handle with indexeddb

---
+++ indexeddb/app.js
@@ -0,0 +1,34 @@
+import Dexie from "https://cdn.jsdelivr.net/npm/dexie@3.0.3/dist/dexie.mjs";
+
+const db = new Dexie("pokemonDB");
+
+db.version(1).stores({
+  pokemon: "++id,name",
+});
+
+db.on("populate", async () => {
+  await db.pokemon.bulkPut([
+    {
+      name: "Bulbasaur",
+      picture:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "Charmander",
+      picture:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "Squirtle",
+      picture:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "Pikachu",
+      picture:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+  ]);
+});
+
+db.open();
