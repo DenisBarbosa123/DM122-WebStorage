a68a3219914e00522de067074efe992a99081c82
Denis Sebastião Costa Barbosa <denis.barbosa@inatel.br>
2022-10-08 12:08:39 -0300

    add cache images

--- cache-api/app.js
+++ cache-api/app.js
@@ -17,16 +17,25 @@ async function fetchFromCache(requestUrl){
     return cachedResponse || null;
 }
 
-async function showPokeData(){
-    const pre = document.querySelector('pre');
-    const img = document.querySelector('img');
-    const details = document.querySelector('details');
-    const requestUrl = 'https://pokeapi.co/api/v2/pokemon/282';
-    const pokeDataResponse = (await fetchFromCache(requestUrl)) || (await fetchFromNetwork(requestUrl));
+async function showPokeData() {
+    const img = document.querySelector("img");
+    const details = document.querySelector("details");
+    const pre = document.querySelector("pre");
+  
+    const pokeNumber = Math.floor(Math.random() * 10) + 1;
+    console.log('PokeNumber: ' + pokeNumber);
+    const requestUrl = `https://pokeapi.co/api/v2/pokemon/${pokeNumber}`;
+    const pokeDataResponse =
+      (await fetchFromCache(requestUrl)) || (await fetchFromNetwork(requestUrl));
     const pokeData = await pokeDataResponse.json();
     pre.textContent = JSON.stringify(pokeData, null, 2);
-    img.src = pokeData.sprites.other["official-artwork"].front_default;
     img.alt = pokeData.name;
     img.title = pokeData.name;
     details.hidden = false;
-}
+  
+    const pokeImgUrl = pokeData.sprites.other["official-artwork"].front_default;
+    const pokeImageRequest =
+    (await fetchFromCache(pokeImgUrl)) || (await fetchFromNetwork(pokeImgUrl));
+    const blob = await pokeImageRequest.blob();
+    img.src = URL.createObjectURL(blob);
+  }
